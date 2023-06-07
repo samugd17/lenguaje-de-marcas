cb845867bed2837cbf7e57cbc1deaf07e8b58287
Samugd17 <samugd17@gmail.com>
2023-06-07 15:25:17 +0100

    Se añaden funciones

--- JavaScript/project5/src/js/main.js
+++ JavaScript/project5/src/js/main.js
@@ -1,11 +1,30 @@
 window.onload = () => {
-    GetData()
+    getData();
+    alertName();
 }
 
-function GetData() {
+function getData() {
     document.querySelector("form").addEventListener("submit", e => {
         e.preventDefault()
         const data = Object.fromEntries(new FormData(e.target))
         console.log(JSON.stringify(data))
+    })
+}
+
+function alertName() {
+    var name_lenght = document.querySelectorAll("#name").length;
+    document.querySelector("form").addEventListener("submit", e => {
+        e.preventDefault()
+    if (name_lenght < 6) {
+        alert("El nombre debe tener más de 6 caracteres")
+    }
+})
+}
+
+function correctEmail() {
+    let mail = document.getElementById("email")
+    document.querySelector("form").addEventListener("submit", e => {
+        e.preventDefault()
+    let regex = /\w+@\w+.com/
     })
 }
